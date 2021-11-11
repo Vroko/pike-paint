beafacc44d23913d652bf5eee606fe7d322801a5
icebit <epicpotatopro@gmail.com>
2021-11-11 15:15:57 -0600

    Set up basic Django server with views and urls

---
+++ pikepaintapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+# Create your views here.
+def index(request):
+    template = loader.get_template('base.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+
+def files(request):
+    template = loader.get_template('files.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
